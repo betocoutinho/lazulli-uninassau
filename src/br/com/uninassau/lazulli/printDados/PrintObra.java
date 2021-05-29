d14d545c2e21d78547f34b41bdd3da84f3860401
betocoutinho <robertocoutinho2207@outlook.com>
2021-05-29 15:07:09 -0300

    Criação do pacote contexto

---
+++ src/br/com/uninassau/lazulli/printDados/PrintObra.java
@@ -0,0 +1,24 @@
+package br.com.uninassau.lazulli.printDados;
+
+import br.com.uninassau.lazulli.entidades.Obra;
+
+import java.util.List;
+
+public class PrintObra {
+
+    public static void print(Obra obra){
+        System.out.println(
+                "COD: " + obra.getCodigoDaObra() + " ::: "
+                + "Empreendimento: " + obra.getNomedaObra()
+                + " End: " + obra.getEndereco()
+                + " Fiscal Responsavel: " + obra.getFiscal().getNomeDoFiscal()
+        );
+    }
+    
+    public static void print(List<Obra> obras){
+        for (Obra obra :
+                obras) {
+            print(obra);
+        }
+    }
+}
