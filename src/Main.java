b0925a0eb5ac47539b2dc43a2cacdce0d6fce3ac
betocoutinho <robertocoutinho2207@outlook.com>
2021-06-02 18:32:49 -0300

    correções

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import br.com.uninassau.lazulli.contexto.ItemContexto;
 import br.com.uninassau.lazulli.menuInicial.MenuInicial;
 
-public class Teste {
+public class Main {
     public static void main(String[] args) {
         MenuInicial.inicio();
 
